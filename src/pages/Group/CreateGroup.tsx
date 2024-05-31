f0991923a018e4f3bf41fcf32af230003cbd4030
Salmane <elmourtajisalmane@gmail.com>
2024-05-31 07:16:39 +0100

    implemented create and delete for group entity

--- src/pages/Group/CreateGroup.tsx
+++ src/pages/Group/CreateGroup.tsx
@@ -1,15 +1,32 @@
-import { FormControl, FormLabel, FormErrorMessage, Stack, Input, Flex, Select, Button, Card, CardBody, CardHeader, Heading } from "@chakra-ui/react"
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Stack,
+  Input,
+  Flex,
+  Select,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Checkbox,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useAxiosFunction from "../../hooks/useAxiosFunction";
-import axios from '../../apis/scholarSync'
+import axios from "../../apis/scholarSync";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
-import {Alert, AlertIcon } from "@chakra-ui/react";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 import { capitalize } from "../../utils/text";
 
 const FORM_VALIDATION = Yup.object().shape({
   name: Yup.string().required(),
-  creation_criteria: Yup.string().oneOf(["RANDOM", "BY_SECTOR", "BY_DEPARTMENT"]).required(),
+  creationCriteria: Yup.string()
+    .oneOf(["RANDOM", "BY_SECTOR", "BY_DEPARTMENT"])
+    .required(),
+  users: Yup.array().required(),
 });
 
 const CreateDepartment = ({ setReload }: any) => {
@@ -25,8 +42,7 @@ const CreateDepartment = ({ setReload }: any) => {
     return () => clearTimeout(timer);
   }, [showSuccess, showError]);
 
-  //------------
-    // select input data
+  // select input data
   const [sectors, setSectors] = useState([]);
   const [departments, setDepartments] = useState([]);
   const [, , , axiosFetch] = useAxiosFunction(axios);
@@ -42,232 +58,236 @@ const CreateDepartment = ({ setReload }: any) => {
       handleResponse: (data: any) => setDepartments(data),
     });
   }, []);
-  //--------------load users when updating creteria and creteriaValue-------------------
-  const [creteria, setCreteria] = useState("");
-  const [creteriaValue, setCreteriaValue] = useState(0);//value of sector or department
+  //--------------load users when updating criteria and criteriaValue-------------------
+  const [criteria, setCreteria] = useState("");
+  const [criteriaValue, setCreteriaValue] = useState<any>(0); //value of sector or department
   const [users, setUsers] = useState([]);
 
-
   useEffect(() => {
-    if(creteria==""){
+    if (criteria == "") {
       setUsers([]);
-    }else{
-      if(creteria == "RANDOM"){
-        console.log("random creteria")
+    } else {
+      if (criteria == "RANDOM") {
+        console.log("random criteria");
         axiosFetch({
           url: "/group/prof",
           method: "get",
           handleResponse: (data: any) => setUsers(data),
         });
-      }else if(creteria == "BY_SECTOR" && creteriaValue!=0){
+      } else if (criteria == "BY_SECTOR" && criteriaValue != 0) {
         axiosFetch({
-          url: `/group/prof/sector/${creteriaValue}`,
+          url: `/group/prof/sector/${criteriaValue}`,
           method: "get",
           handleResponse: (data: any) => setUsers(data),
         });
-      }else if(creteria == "BY_DEPARTMENT" && creteriaValue!=0){
+      } else if (criteria == "BY_DEPARTMENT" && criteriaValue != 0) {
         axiosFetch({
-          url: `/group/prof/department/${creteriaValue}`,
+          url: `/group/prof/department/${criteriaValue}`,
           method: "get",
           handleResponse: (data: any) => setUsers(data),
         });
       }
     }
-  }, [creteria, creteriaValue])
-  
+  }, [criteria, criteriaValue]);
 
-
-
-  //formsubmission
+  //form submission
   const onSubmit = (values: any, actions: any) => {
     axiosFetch({
-      url:'/department',
-      method:'post',  
+      url: "/group/create",
+      method: "post",
       data: values,
       handleResponse: () => {
         actions.setSubmitting(false);
-        setShowSuccess(true)
-        actions.resetForm()
-        setReload((prev: number)=>prev+1)
+        setShowSuccess(true);
+        actions.resetForm();
+        setReload((prev: number) => prev + 1);
+        actions.resetForm();
+        setCreteria("");
+        setCreteriaValue(0);
       },
       handleError: () => {
         actions.setSubmitting(false);
-        setShowError(true)
-        actions.resetForm()
+        setShowError(true);
+        actions.resetForm();
       },
-    })
+    });
   };
 
   return (
     <Formik
       initialValues={{
         name: "",
+        creationCriteria: '',
+        users: [],
       }}
       onSubmit={onSubmit}
       validationSchema={FORM_VALIDATION}
     >
       {(props) => (
-          <Form>
-            <Flex flexDirection={"column"} gap={3}>
-                <Alert status="success" variant="left-accent" hidden={!showSuccess}>
-                <AlertIcon />
-                Department created successfully !
-                </Alert>
-                <Alert status="error" variant="left-accent" hidden={!showError}>
-                <AlertIcon />
-                Something went wrong, try again
-                </Alert>
-                <Card shadow={'none'} borderRadius={'20px'}>
-                    <CardHeader>
-                    <Heading as="h3" size={"md"}>
-                        Create New Department
-                    </Heading>
-                    </CardHeader>
-                    <CardBody>
-                      <Stack
-                      w={"100%"}
-                      spacing={4}
-                      alignItems={"start"}
-                      direction={["column", "column", "row"]}
-                      >
-                        <Field name="name">
-                          {({ field, form }: any) => (
-                          <FormControl
-                              isInvalid={form.errors.name && form.touched.name}
+        <Form>
+          <Flex flexDirection={"column"} gap={3}>
+            <Alert status="success" variant="left-accent" hidden={!showSuccess}>
+              <AlertIcon />
+              Department created successfully !
+            </Alert>
+            <Alert status="error" variant="left-accent" hidden={!showError}>
+              <AlertIcon />
+              Something went wrong, try again
+            </Alert>
+            <Card shadow={"none"} borderRadius={"20px"}>
+              <CardHeader>
+                <Heading as="h3" size={"md"}>
+                  Create New Group
+                </Heading>
+              </CardHeader>
+              <CardBody>
+                <Stack gap={4}>
+                  <Stack
+                    w={"100%"}
+                    spacing={4}
+                    alignItems={"start"}
+                    direction={["column", "column", "row"]}
+                    gap={3}
+                  >
+                    <Field name="name">
+                      {({ field, form }: any) => (
+                        <FormControl
+                          isInvalid={form.errors.name && form.touched.name}
+                        >
+                          <FormLabel>Name</FormLabel>
+                          <Input
+                            isRequired={true}
+                            {...field}
+                            onChange={form.handleChange}
+                            variant={"auth"}
+                            type="text"
+                          />
+                          <FormErrorMessage>
+                            {form.errors.name}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="creationCriteria">
+                      {({ field, form }: any) => (
+                        <FormControl>
+                          <FormLabel> Group Creation Creteria</FormLabel>
+                          <Select
+                            isInvalid={
+                              form.errors.creation_criteria &&
+                              form.touched.creation_criteria
+                            }
+                            variant={"auth"}
+                            isRequired={true}
+                            {...field}
+                            onChange={(e) => {
+                              form.handleChange(e);
+                              setCreteria(e.target.value || "");
+                            }}
+                            placeholder="Select a criteria"
                           >
-                              <FormLabel>Name</FormLabel>
-                              <Input
-                              isRequired={true}
-                              {...field}
-                              onChange={form.handleChange}
-                              variant={"auth"}
-                              type="text"
-                              />
-                              <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                          </FormControl>
-                          )}
-                        </Field>
-                        <Field name="creation_creteria">
-                            {({ field, form }: any) => (
-                              <FormControl>
-                                <FormLabel> Group Creation Creteria</FormLabel>
-                                <Select
-                                  isInvalid={
-                                    form.errors.creation_creteria && form.touched.creation_creteria
-                                  }
-                                  variant={"auth"}
-                                  isRequired={true}
-                                  {...field}
-                                  onChange={(e) => {
-                                    form.handleChange(e);
-                                    setCreteria(e.target.value || "");
-                                  }}                                    
-                                  placeholder="Select a creteria"
-                                >
-                                    <option value="RANDOM">Random</option>
-                                    <option value="BY_SECTOR">By Sector</option>
-                                    <option value="BY_DEPARTMENT">By Department</option>
-                                </Select>
-                                <FormErrorMessage>
-                                  {form.errors.creation_creteria}
-                                </FormErrorMessage>
-                              </FormControl>
-                            )}
-                        </Field>
-                      </Stack>
+                            <option value="RANDOM">Random</option>
+                            <option value="BY_SECTOR">By Sector</option>
+                            <option value="BY_DEPARTMENT">By Department</option>
+                          </Select>
+                          <FormErrorMessage>
+                            {form.errors.creation_criteria}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Stack>
 
-                      {/* display sectors or department dynamically */}
-                      <Stack
-                      w={"100%"}
-                      spacing={4}
-                      alignItems={"start"}
-                      direction={["column", "column", "row"]}
-                      >                          
-                        {creteria == "RANDOM"}
-                        {creteria == "BY_SECTOR" &&
-                        <Field name="sectorId">
-                        {({ field, form }: any) => (
-                          <FormControl
-                            isInvalid={form.errors.sectorId && form.touched.sectorId}
+                  {/* display sectors or department dynamically */}
+                  <Stack
+                    w={"100%"}
+                    spacing={4}
+                    alignItems={"start"}
+                    direction={["column", "column", "row"]}
+                  >
+                    {criteria == "BY_SECTOR" && (
+                        <FormControl>
+                          <FormLabel>Sector</FormLabel>
+                          <Select
+                            isRequired={true}
+                            onChange={(e) => {
+                              setCreteriaValue(e.target.value || 0);
+                            }}
+                            variant={"auth"}
+                            placeholder="Select a Sector"
                           >
-                            <FormLabel>Sector</FormLabel>
+                            {sectors?.map((sector: any, index: number) => (
+                              <option key={index} value={sector.id}>
+                                {capitalize(sector.name)}
+                              </option> 
+                            ))}
+                          </Select>
+                        </FormControl>
+                    )}
+                    {criteria == "BY_DEPARTMENT" && (
+                          <FormControl>
+                            <FormLabel>Department</FormLabel>
                             <Select
+                              variant={"auth"}
                               isRequired={true}
-                              {...field}
                               onChange={(e) => {
-                                form.handleChange(e);
                                 setCreteriaValue(e.target.value || 0);
                               }}
-                              variant={"auth"}
-                              placeholder="Select a Sector"
-                              >
-                              {sectors?.map((sector: any, index: number) => (
-                                <option key={index} value={sector.id}>
-                                  {capitalize(sector.name)}
-                                </option>
-                              ))}
-                            </Select>
-                            <FormErrorMessage>
-                              {form.errors.sectorId}
-                            </FormErrorMessage>
-                          </FormControl>
-                        )}
-                        </Field>}
-                        {creteria == "BY_DEPARTMENT" &&
-                        <Field name="departmentId">
-                          {({ field, form }: any) => (
-                            <FormControl>
-                              <FormLabel>Department</FormLabel>
-                              <Select
-                                isInvalid={
-                                  form.errors.departmentId && form.touched.departmentId
-                                }
-                                variant={"auth"}
-                                isRequired={true}
-                                {...field}
-                                onChange={(e) => {
-                                  form.handleChange(e);
-                                  setCreteriaValue(e.target.value || 0);
-                                }}
-                                placeholder="Select a Department"
-                              >
-                                {departments?.map((department: any, index: number) => (
+                              placeholder="Select a Department"
+                            >
+                              {departments?.map(
+                                (department: any, index: number) => (
                                   <option key={index} value={department.id}>
                                     {capitalize(department.name)}
                                   </option>
-                                ))}
-                              </Select>
-                              <FormErrorMessage>
-                                {form.errors.departmentId}
-                              </FormErrorMessage>
-                            </FormControl>
-                          )}
-                        </Field>}
-                      </Stack>
+                                )
+                              )}
+                            </Select>
+                          </FormControl>
+                    )}
+                  </Stack>
 
-                      {/* display proffesors */}
-                      {users && 
-                      }
-
-                  </CardBody>
-                </Card>
-                <Button
-                size="md"
-                height="48px"
-                width="200px"
-                variant={"navy"}
-                w={"100%"}
-                mt={1}
-                type="submit"
-                isLoading={props.isSubmitting}
-                >
-                Create
-                </Button>
-            </Flex>
-            </Form>
+                  {/* display professors */}
+                  {users.length > 0 && (
+                    <Stack gap={4}>
+                      <Heading as="h6" size={"sm"}>
+                        Select professors for the group
+                      </Heading>
+                      <Flex direction={"row"} gap={"30px"}>
+                        {users.map((user: any, index: number) => (
+                          <Field name="users" key={index}>
+                            {({ field }: any) => (
+                              <Checkbox w={"20%"} {...field} value={user.id}>
+                                {capitalize(user.firstName) +
+                                  " " +
+                                  capitalize(user.lastName)}
+                              </Checkbox>
+                            )}
+                          </Field>
+                        ))}
+                      </Flex>
+                    </Stack>
+                  )}
+                </Stack>
+              </CardBody>
+            </Card>
+            <Button
+              size="md"
+              height="48px"
+              width="200px"
+              variant={"navy"}
+              w={"100%"}
+              mt={1}
+              type="submit"
+              isLoading={props.isSubmitting}
+            >
+              Create
+            </Button>
+          </Flex>
+        </Form>
       )}
     </Formik>
   );
-}
+};
 
-export default CreateDepartment
+export default CreateDepartment;
--- src/pages/Group/GroupsTable.tsx
+++ src/pages/Group/GroupsTable.tsx
@@ -1,130 +1,118 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
+    Card, CardHeader, CardBody,
+    TableContainer, Table, Thead, Tbody, Tr, Th, Td,
     Heading,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th, 
-    Td,
-    TableContainer,
     IconButton,
     useColorModeValue,
     Alert,
     AlertIcon,
+    Tag
   } from "@chakra-ui/react";
-  import { MdEdit } from "react-icons/md";
   import { MdOutlineDeleteOutline } from "react-icons/md";
   import { capitalize } from "../../utils/text";
-  import ModalDelete from "./ModalDelete";
   import { useEffect, useState } from "react";
-import ModalUpdate from "./ModalUpdate";
-import DeleteModal from "../../components/Modal/DeleteModal";
+  import DeleteModal from "../../components/Modal/DeleteModal";
+
   const GroupsTable = ({ groups, setReload}: any) => {
   const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
   
-  const [selectedGroup, setSelectedGroup] = useState<Group>(null);
   const [selectedGroupId, setSelectedGroupId] = useState(0);
 
   // Allert
   const [deleteSuccess, setDeleteSuccess] = useState(false);
-  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
   useEffect(() => {
     const timer = setTimeout(() => {
-      setUpdateSuccess(false);
       setDeleteSuccess(false);
+      setShowError(false);
     }, 3500);
 
     return () => clearTimeout(timer);
-  }, [updateSuccess, deleteSuccess]);
+  }, [deleteSuccess, showError]);
 
     return (
       <>
-      <Alert status="success" variant="left-accent" hidden={!deleteSuccess}>
-      <AlertIcon />
-      Group deleted successfully !
-      </Alert>
-      <Alert status="success" variant="left-accent" hidden={!updateSuccess}>
-      <AlertIcon />
-      Group updated successfully !
-      </Alert>
-      <Card shadow={'none'} borderRadius={'20px'}>
-        <CardHeader>
-          <Heading as="h3" size={"md"}>
-            Groups
-          </Heading>
-        </CardHeader>
-        <CardBody>
-
-          <TableContainer>
-            <Table variant="simple" size={"md"}>
-              <Thead>
-                <Tr>
-                  <Th borderColor={borderColor}>Id</Th>
-                  <Th borderColor={borderColor}>Name</Th>
-                  <Th borderColor={borderColor}>Creation Criteria</Th>
-                  <Th borderColor={borderColor}></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {groups?.map((group: Group, index: number) => (
-                  <Tr key={index}>
-                    <Td>{groups.id}</Td>
-                    <Td>{capitalize(group.name)}</Td>
-                    <Td>{group.creation_criteria}</Td>
-                    <Td display={"flex"} gap={1}>
-                      <IconButton
-                        onClick={() => setSelectedGroup(group)}
-                        size={"sm"}
-                        variant="lightBrand"
-                        aria-label="update"
-                        icon={<MdEdit size={"18px"} />}
-                      />
-                      <IconButton
-                        onClick={() => setSelectedGroupId(group.id)}
-                        size={"sm"}
-                        variant="lightBrand"
-                        aria-label="delete"
-                        icon={<MdOutlineDeleteOutline size={"18px"} />}
-                      />
-                    </Td>
+        <Alert status="success" variant="left-accent" hidden={!deleteSuccess}>
+          <AlertIcon />
+          Group deleted successfully !
+        </Alert>
+        <Alert status="error" variant="left-accent" hidden={!showError}>
+          <AlertIcon />
+          Something went wrong, try again
+        </Alert>
+        <Card shadow={"none"} borderRadius={"20px"}>
+          <CardHeader>
+            <Heading as="h3" size={"md"}>
+              Groups
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <TableContainer>
+              <Table variant="simple" size={"md"}>
+                <Thead>
+                  <Tr>
+                    <Th borderColor={borderColor}>Id</Th>
+                    <Th borderColor={borderColor}>Name</Th>
+                    <Th borderColor={borderColor}>Creation Criteria</Th>
+                    <Th borderColor={borderColor}></Th>
                   </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </CardBody>
+                </Thead>
+                <Tbody>
+                  {groups?.map((group: Group, index: number) => (
+                    <Tr key={index}>
+                      <Td>{group.id}</Td>
+                      <Td>{capitalize(group.name)}</Td>
+                      <Td>
+                        <Tag
+                          variant={"solid"}
+                          justifyContent={"center"}
+                          w={"115px"}
+                          colorScheme={
+                            group.creationCriteria == "BY_SECTOR"
+                              ? "green"
+                              : "blue"
+                          }
+                        >
+                          {group.creationCriteria == "BY_SECTOR"
+                            ? "Sector"
+                            : "Department"}
+                        </Tag>
+                      </Td>
+                      <Td display={"flex"} gap={1}>
+                        <IconButton
+                          onClick={() => setSelectedGroupId(group.id)}
+                          size={"sm"}
+                          variant="lightBrand"
+                          aria-label="delete"
+                          icon={<MdOutlineDeleteOutline size={"18px"} />}
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </CardBody>
 
-        {selectedGroup != null &&  
-        <ModalUpdate
-          group={selectedGroup}
-          onClose={() => setSelectedGroup<any>(null)}
-          setReload={setReload}
-          setUpdateSuccess={setUpdateSuccess}
-        />}
-
-        {selectedGroupId!=0 &&
-        <DeleteModal
-          url={"/group/"+selectedGroupId}
-          message={"Are you sure to delete this group?"}
-          onClose={() => setSelectedGroupId(0)}
-          setReload={setReload}
-          setDeleteSuccess={setDeleteSuccess}
-        />
-        }
-      </Card></>
-
-
-
+          {selectedGroupId != 0 && (
+            <DeleteModal
+              url={"/group/" + selectedGroupId}
+              message={"Are you sure to delete this group?"}
+              onClose={() => setSelectedGroupId(0)}
+              setReload={setReload}
+              setDeleteSuccess={setDeleteSuccess}
+              setShowError={setShowError}
+            />
+          )}
+        </Card>
+      </>
     );
   };
   
   type Group = {
     id: number,
     name: string,
-    creation_criteria: string,
+    creationCriteria: string,
   }
   
   export default GroupsTable;
